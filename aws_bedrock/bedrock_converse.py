1353d723ef29609cb4f4330f51ec11ad8a53b888
ideaweaver-ai <plakhera@ideaweaver.ai>
2026-07-23 11:02:29 -0700

    Create bedrock_converse.py

---
+++ aws_bedrock/bedrock_converse.py
@@ -0,0 +1,22 @@
+import boto3
+
+client = boto3.client(
+    "bedrock-runtime",
+    region_name="us-east-1"
+)
+
+response = client.converse(
+    modelId="amazon.nova-lite-v1:0",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {
+                    "text": "Explain Amazon Bedrock in simple language."
+                }
+            ]
+        }
+    ]
+)
+
+print(response["output"]["message"]["content"][0]["text"])
